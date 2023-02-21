2c43b5f78903d039d885e9c3640d616b587b02c5
AlexandrKozhekin <alexandrkozhekin@gmail.com>
2023-02-21 16:55:33 +0300

    Refactoring repository interface for JPA

--- src/main/java/sia/tacocloud/tacos/core/repository/IngredientRepository.java
+++ src/main/java/sia/tacocloud/tacos/core/repository/IngredientRepository.java
@@ -1,14 +1,17 @@
 package sia.tacocloud.tacos.core.repository;
 
+import org.springframework.data.repository.CrudRepository;
+
 import sia.tacocloud.tacos.core.domain.Ingredient;
 
 import java.util.Optional;
 
-public interface IngredientRepository {
+public interface IngredientRepository extends CrudRepository<Ingredient, String> {
 
-    Iterable<Ingredient> findAll();
+//    Iterable<Ingredient> findAll();
+//
+//    Optional<Ingredient> findById(String id);
+//
+//    Ingredient save(Ingredient ingredient);
 
-    Optional<Ingredient> findById(String id);
-
-    Ingredient save(Ingredient ingredient);
 }
--- src/main/java/sia/tacocloud/tacos/core/repository/OrderRepository.java
+++ src/main/java/sia/tacocloud/tacos/core/repository/OrderRepository.java
@@ -1,9 +1,19 @@
 package sia.tacocloud.tacos.core.repository;
 
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
 import sia.tacocloud.tacos.core.domain.TacoOrder;
 
-public interface OrderRepository {
+import java.util.Date;
+import java.util.List;
+
+public interface OrderRepository extends CrudRepository<TacoOrder, Long> {
 
     TacoOrder save(TacoOrder order);
+
+    List<TacoOrder> findByDeliveryZip(String deliveryZip);
+
+    List<TacoOrder> readOrdersByDeliveryZipAndPlacedAtBetween(String deliveryZip, Date startDate, Date endDate);
 
 }
